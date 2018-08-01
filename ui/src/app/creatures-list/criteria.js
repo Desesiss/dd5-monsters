34a9dd5f63392132fd09da1928ee1b42c509f7de
Desesiss <heolio@hotmail.fr>
2018-08-01 16:23:27 +0200

    Improve usability of search field

--- ui/src/app/creatures-list/criteria.js
+++ ui/src/app/creatures-list/criteria.js
@@ -6,6 +6,7 @@ class Search extends Component {
     super(props);
 
     this._executeSearch = this._executeSearch.bind(this);
+    this._handleKeyPress = this._handleKeyPress.bind(this);
 
     this.state = {
       filter: ''
@@ -18,16 +19,24 @@ class Search extends Component {
     }
   }
 
+  _handleKeyPress(e){
+    if (e.key === 'Enter') {
+      this. _executeSearch();
+    }
+  }
+
   render() {
     return (
       <div className='paragraph'>
         <div className='dd-aligned'>
+          <i className='material-icons' onClick={this._executeSearch}>search</i>
           <input
             type='text'
             onChange={e => this.setState({ filter: e.target.value })}
             value={this.state.filter}
+            placeholder={'Rechercher une créature...'}
+            onKeyPress={this._handleKeyPress}
           />
-          <i className='material-icons' onClick={this._executeSearch}>search</i>
         </div>
       </div>
     )
